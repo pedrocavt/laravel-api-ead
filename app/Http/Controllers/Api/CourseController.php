3f7b0a79fea81df53d6a427256429f927605822f
Pedro Vitor <pedrocavt@gmail.com>
2022-01-14 23:24:09 -0300

    api course show

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -14,4 +14,10 @@ class CourseController extends Controller
 
         return CourseResource::collection($courses);
     }
+
+    public function show($id) {
+        $course = Course::findOrFail($id);
+
+        return new CourseResource($course);
+    }
 }
